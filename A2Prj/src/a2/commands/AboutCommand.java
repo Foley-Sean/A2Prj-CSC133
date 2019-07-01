f68b043a0579f3850fba1413023927ba38486847
Sean Foley <Sean Foley@SeanPC.attlocal.net>
2019-07-01 08:03:20 -0700

    About updated to credit for song

--- A2Prj/src/a2/commands/AboutCommand.java
+++ A2Prj/src/a2/commands/AboutCommand.java
@@ -13,8 +13,8 @@ public class AboutCommand extends Command {
 	@Override 
 	public void actionPerformed(ActionEvent e) {
 		
-		Dialog.show("About", "This is my Asteroid game coded for CSC133 in the Summer of 2019. Thanks for checking it out."
-				+ "Version .5", "OK", "Cancel");
+		Dialog.show("About", "This is my Asteroid game coded for CSC133 in the Summer of 2019. Thanks for checking it out." + "\n" +
+				" Version 1.0.1" + "\n" + "Sound by Sam Hendryx. Song: Half Step Tripple Threat - no drums.mp3 ", "OK", "Cancel");
 		
 		//System.out.println("About");
 	}
